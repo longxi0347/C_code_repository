7abc662f5df8c0d4d5aa897d1fd1beeabbb66a6e
longxixyz <longxi347@gmail.com>
2023-10-16 23:46:57 +0800

    代码练习

---
+++ test_23_10_16/test_23_10_16/test.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//Å£¿ÍÍø BC64 Å£Å£µÄ¿ìµÝ
+//#include <stdio.h>
+//int main()
+//{
+//    float a = 0;
+//    char b = 0;
+//    float val = 0;
+//    scanf("%f %c", &a, &b);
+//    if (a <= 1)
+//        val = 20;
+//    else
+//    {
+//        val = (int)a + 19;
+//        if (a > (int)a)
+//            val++;
+//    }
+//    if (b == 'y')
+//        val += 5;
+//    printf("%.f", val);
+//    return 0;
+//}
