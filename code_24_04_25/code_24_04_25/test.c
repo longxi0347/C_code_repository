a1feaa239bbc21dbf8e2e0ed7c0b9f166874d55d
longxixyz <longxi347@gmail.com>
2024-04-26 08:58:32 +0800

    代码练习

---
+++ code_24_04_25/code_24_04_25/test.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+/*
+//leetcode 4月25日每日一题 2739
+#include <stdio.h>
+int main()
+{
+	int mainTank = 0;
+	int additionalTank = 0;
+	scanf("%d %d", &mainTank, &additionalTank);
+	int cost = 0;
+	int distance = 0;
+	while (mainTank != 0)
+	{
+		cost++;
+		mainTank--;
+		distance += 10;
+		if (cost % 5 == 0 && additionalTank != 0)
+		{
+			mainTank++;
+			additionalTank--;
+		}
+	}
+	printf("%d", distance);
+	return 0;
+}
+*/
